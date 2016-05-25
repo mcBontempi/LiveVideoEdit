5b8fdd37e1961a0b9e7114d98d08a5d852f40e70
Daren David Taylor <mcbontempi@darens-MacBook-Pro-2.local>
2016-05-25 08:35:37 +0100

    basic layout

--- LiveVideoEdit/LiveVideoEdit/AppDelegate.swift
+++ LiveVideoEdit/LiveVideoEdit/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         BuddyBuildSDK.setup()
         
-        // Override point for customization after application launch.
+        application.statusBarHidden = true;
         return true
     }
 
---
+++ LiveVideoEdit/LiveVideoEdit/RecordViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RecordViewController.swift
+//  LiveVideoEdit
+//
+//  Created by Daren David Taylor on 24/05/2016.
+//  Copyright © 2016 HolidayExtras. All rights reserved.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
