11e06b40ffeb777fd41c1bcea7f7514450e880bb
Daren David Taylor <mcbontempi@darens-MacBook-Pro-2.local>
2016-05-25 11:22:35 +0100

    button logic done

--- LiveVideoEdit/LiveVideoEdit/RecordViewController.swift
+++ LiveVideoEdit/LiveVideoEdit/RecordViewController.swift
@@ -10,85 +10,120 @@ import UIKit
 import PBJVision
 
 class RecordViewController: UIViewController {
-
+    
+    @IBOutlet weak var startStopButton: UIButton!
+    @IBOutlet weak var recordButton: UIView!
+  
+    var started:Bool = false
+    var recording:Bool = false
+    
+    @IBAction func longPressPressed(gestureRecognizer: UILongPressGestureRecognizer) {
+        
+        switch (gestureRecognizer.state) {
+        case .Began:
+            if self.recording == false {
+                
+                self.recording = true
+                
+                PBJVision.sharedInstance().startVideoCapture()
+                
+                print ("start")
+                
+            }
+            else {
+                self.recording = false
+                
+                PBJVision.sharedInstance().resumeVideoCapture()
+           
+                print ("resume")
+                
+            }
+        case .Ended, .Cancelled, .Failed:
+            PBJVision.sharedInstance().pauseVideoCapture()
+            
+            print ("pause")
+            
+        default:
+            break;
+        }
+    }
+    
+    
+    
+    
+    
+    
+    @IBAction func startStopPressed(sender: AnyObject) {
+        
+        if self.started == false {
+            self.started = true
+            
+            self.recordButton.hidden = false
+            
+        }
+        else {
+            self.performSegueWithIdentifier("previewSegue", sender: nil)
+        }
+        
+    }
+    
     @IBOutlet weak var previewView: UIView!
     
     var previewLayer: AVCaptureVideoPreviewLayer!
     
-    
     func createPreviewView() {
         self.previewLayer = PBJVision.sharedInstance().previewLayer
-        
         self.previewLayer.backgroundColor = UIColor.blackColor().CGColor
         self.previewView.backgroundColor = UIColor.blackColor()
-        
         self.previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
         self.previewView.layer.addSublayer(self.previewLayer)
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         self.createPreviewView()
-        
-    PBJVision.sharedInstance().startPreview()
+        PBJVision.sharedInstance().startPreview()
     }
     
     override func prefersStatusBarHidden() -> Bool {
         return false
     }
     
-    
-    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
-     
-        
-    }
-    
-    
     override func willAnimateRotationToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
         
         let vision = PBJVision.sharedInstance()
-        
-        
         var visionOrientation:PBJCameraOrientation?
         
-        
         switch(toInterfaceOrientation) {
-            
         case .Unknown:
             visionOrientation =  .Portrait
         case .Portrait:
-               visionOrientation =  .Portrait
+            visionOrientation =  .Portrait
         case .PortraitUpsideDown:
-               visionOrientation =  .PortraitUpsideDown
+            visionOrientation =  .PortraitUpsideDown
         case .LandscapeLeft:
-               visionOrientation = .LandscapeLeft
+            visionOrientation = .LandscapeLeft
         case .LandscapeRight:
-               visionOrientation = .LandscapeRight
-            
+            visionOrientation = .LandscapeRight
         }
-        
-        
         vision.cameraOrientation = visionOrientation!
-        
     }
     
-    
     func updatePreviewLayer() {
-        
         if let previewView = self.previewView {
             if let previewLayer = self.previewLayer {
                 previewLayer.frame = previewView.frame
-                
             }
         }
-        
     }
-
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         self.updatePreviewLayer()
     }
+    
+    
+    
 }
+
+
